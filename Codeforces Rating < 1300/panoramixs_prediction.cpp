5884f724a8b83fa7f1909f6209664d8ac5123b15
Falgun Marothia <fmfalgunmarothia@gmail.com>
2024-07-03 07:52:43 -0400

    Auto-commit: (date '+%Y-%m-%d %H:%M:%S')

---
+++ Codeforces Rating < 1300/panoramixs_prediction.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+
+	int n, m;
+	cin >> n >> m;
+	bool flag = false;
+	for (int i=n+1; i<m; i++){
+		flag = true;
+		for(int j=2; j <i; j++){
+			if (i%j == 0) flag = false;
+			cout << i << " " << j << " " <<flag << endl;
+		}	
+		if (flag) break;
+	}
+
+	cout << flag; 
+
+	return 0;
+}
