130bb6bb26ffd3ddfe4f79d4030ce759cf2dc786
Falgun Marothia <fmfalgunmarothia@gmail.com>
2024-07-05 02:03:13 -0400

    Auto-commit: (date '+%Y-%m-%d %H:%M:%S')

--- Codeforces Rating < 1300/ultra_fast_mathematician.cpp
+++ Codeforces Rating < 1300/ultra_fast_mathematician.cpp
@@ -5,19 +5,19 @@ using namespace std;
 int main(void){
 
 	string str1, str2, result;
-	cin << str1 << str2;
+	cin >> str1 >> str2;
 
 	int count = 0, len = str1.length();
 
 	while(count == len){
 
 		if (str1[count] == str2[count])
-				result += '0';
-		else result += '1';
+				result += "0";
+		else result += "1";
 		count++;
 
 	}
-	cout << result;
+	cout << result << "ok\n";
 
 	return 0;
 }
