7d3086585c59c1085ea6a78feebc115d4c7071d0
Falgun Marothia <fmfalgunmarothia@gmail.com>
2024-12-30 19:45:19 +0530

    easy tha; siply applied concept of if else and while loop, but can be optimized more !

---
+++ Codeforces Rating < 1300/translation.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    
+	string s, t;
+	cin >> s >> t;	
+
+	bool flag = true;
+	if (s.length() != t.length()) flag = false;
+	else{
+		
+		int lenn = s.length(), iterator = 0;
+		while(iterator < lenn && flag){
+			if (s[iterator] != t[lenn - iterator -1]) flag = false;
+			iterator ++;
+		}
+	}
+
+	cout << (flag?"YES":"NO");
+
+    return 0;
+}
+
+
