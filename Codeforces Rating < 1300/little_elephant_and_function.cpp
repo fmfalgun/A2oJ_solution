c24f01f509d32dff13577f895d1666d7121d7986
Falgun Marothia <fmfalgunmarothia@gmail.com>
2025-01-07 15:42:15 +0530

    learned that i should think about problem statement seriously before working on it ! and I also need to work on recursion and pointers understanding

--- Codeforces Rating < 1300/little_elephant_and_function.cpp
+++ Codeforces Rating < 1300/little_elephant_and_function.cpp
@@ -1,35 +1,11 @@
 #include <iostream>
 
 using namespace std;
+int main (void){
 
-void print_perm(int* arr,int size){
-	cout << "\narray = ";
-	for (int i=0; i<size; i++)
-		cout << arr[i] << " ";
-	cout << endl;
-}
-
-void recursion(int x, int* arr, int size){
-	if (x==0) return;
-	recursion(x-1,arr,size);
-
-	int temp = arr[x-1];
-	cout << x << " " << temp << endl;
-	print_perm(arr,size);
-	arr[x-1] = arr[x];
-	arr[x] = temp;
-}
-
-int main() {
-    
 	int n;
 	cin >> n;
-	int perm[n];
-	for (int i=0;i<n; i++) perm[i]=i+1;
-
-	recursion (n, perm,n);
-	print_perm(perm,n);
-    return 0;
+	cout << n << " ";
+	for (int i=1; i<n; i++) cout << i << " ";
+	return 0;
 }
-
-
