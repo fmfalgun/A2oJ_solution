4b6457c409a138290be5c0a8069d35e0386216e8
Falgun Marothia <fmfalgunmarothia@gmail.com>
2025-01-12 15:34:55 +0530

    Problem 61: improved attempt, with reduced time computation

--- Codeforces Rating < 1300/pashmak_n_flower.cpp
+++ Codeforces Rating < 1300/pashmak_n_flower.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <climits>
+#include <algorithm>
 
 using namespace std;
 
@@ -15,17 +16,17 @@ int main() {
 		flwr.push_back(temp);
 	}
 
-	long long int max_diff = INT_MIN;
+	sort(flwr.begin(), flwr.end());
+	long long int max_diff = flwr[flwr.size()-1]-flwr[0];
 	
-	for(int i=0; i<flwr.size(); i++) 
-		for(int j=i; j<flwr.size(); j++)
-			if (abs(flwr[i]-flwr[j]) > max_diff) max_diff = abs(flwr[i]-flwr[j]);
+	long long int nn, minValueCount=0, maxValueCount=0;
 
-	long long int perm=0;
-	for(int i=0; i<flwr.size(); i++)
-		for(int j=i; j<flwr.size(); j++)
-			if (abs(flwr[i]-flwr[j]) == max_diff) perm++;
-	
+	for(int i=0; i<flwr.size(); i++){
+		if (flwr[i] == flwr[0]) minValueCount++;
+		if (flwr[i] == flwr[flwr.size()-1]) maxValueCount++;
+	}
+	nn = flwr.size();
+	long long int perm=(max_diff==0)?((nn*(nn-1)/2)):maxValueCount * minValueCount;
 	cout << max_diff << " " << perm;
     return 0;
 }
