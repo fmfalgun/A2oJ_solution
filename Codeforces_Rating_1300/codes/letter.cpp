a2be3dc52eab427d7335934664c3f24254d09516
fm <fmk45902@gmail.com>
2026-09-17 16:24:13 +0530

    added commits

--- Codeforces_Rating_1300/codes/letter.cpp
+++ Codeforces_Rating_1300/codes/letter.cpp
@@ -5,7 +5,8 @@
 using namespace std;
 
 int main(void){
-    
+
+    // saving all inputs in unordered map which is hash based list, with search time complexity of O(1)
     unordered_map<char, int> freq;
     string input;
     getline(cin,input);
@@ -15,9 +16,12 @@ int main(void){
         else freq[c] = 1;
     }
 
+    // clearing the input string to reuse it for the second input    
     input.clear();
     getline(cin, input);
     bool feasible = true;
+    
+    // main validation logic to generate the response
     for (char c : input){
         if (freq[c] > 0) continue;
         else{
